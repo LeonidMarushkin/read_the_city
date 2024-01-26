9a7d9d16e1f89036fb42f54ee2b4255195329ef7
LeonidMarushkin <139209971+LeonidMarushkin@users.noreply.github.com>
2024-01-26 20:23:29 +0300

    homework done

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <!-- <router-link to="/">Home</router-link> | -->
+  </nav>
+  <router-view />
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="main">
+    <HeaderComponent />
+    <div class="cardFrame">
+      <BookCardVue v-for="(bookData, index) in bookCardData" v-bind:key="index" :author="bookData.author"
+        :title="bookData.title" :imageSource="bookData.imageSource" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BookCardVue from '@/components/elements/BookCard.vue'
+import HeaderComponent from '@/components/blocks/HeaderComponent.vue';
+// import { ref } from 'vue'
+export default {
+  name: 'MainPage',
+  components: {
+    BookCardVue,
+    HeaderComponent
+  },
+  data() {
+    return {
+      bookCardData: [
+        {
+          author: 'Франк Экстанази',
+          title: 'Skyrim. Как продать игру 10 раз',
+          imageSource: require('@/assets/img/image1.png')
+        },
+        {
+          author: 'Сергей Анисимов',
+          title: 'Поисковое продвижение сайтов',
+          imageSource: require('@/assets/img/image2.png')
+        },
+        {
+          author: 'Татьяна Магарина',
+          title: 'Английский для дизайнеров',
+          imageSource: require('@/assets/img/image3.png')
+        },
+        {
+          author: 'Шон Адамс',
+          title: 'Дизайн и цвет. Практикум',
+          imageSource: require('@/assets/img/image4.png')
+        },
+      ]
+    }
+  },
+  props: {
+  },
+  setup() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cardFrame {
+  display: flex;
+  align-items: flex-start;
+  gap: 26px;
+}
+</style>
